2b1c91a517f2271b9ad0921990063e4806b1e4b6
Mario Ossandón Z <mario.hdoz1@gmail.com>
2020-09-20 18:42:33 -0300

    Se modifica ruta para posts

--- posts/index.js
+++ posts/index.js
@@ -17,11 +17,7 @@ app.use(cors());
 const posts = {};
 const PORT = 4000;
 
-app.get('/posts', (req, res)=>{
-    res.send(posts);
-});
-
-app.post('/posts', async (req, res)=>{
+app.post('/posts/create', async (req, res)=>{
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
